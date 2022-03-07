5779930dc051fd684d6493cd84305580cb188f44
mikewebb70 <mikewebb@live.com.au>
2022-03-07 15:57:14 +0800

    more tweeks, nord themes and nvim

--- neovim/.config/nvim/lua/user/plugins.lua
+++ neovim/.config/nvim/lua/user/plugins.lua
@@ -42,6 +42,7 @@ packer.init {
 return packer.startup(function(use)
   -- My plugins here
   use "wbthomason/packer.nvim" -- Have packer manage itself
+  use 'neovim/nvim-lspconfig' -- Collection of configurations for the built-in LSP client
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
   use "windwp/nvim-autopairs" -- Autopairs, integrates with both cmp and treesitter
